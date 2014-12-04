b647c2fd721654fbda0755b187804a8c644727c8
J David Smith <emallson@archlinux.us>
2014-12-04 15:34:33 -0500

    Added logging.

--- server/serve.c
+++ server/serve.c
@@ -1,28 +1,47 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 
 #include "requests.h"
 
+void log_req(store_request* req, bool success) {
+  printf("Secret Key = %d\n", req->base.secret);
+  printf("Request Type = %s\n", req_name_from_type(req->base.type));
+  if(req->base.type == REQ_LIST) {
+    printf("Filename = NONE\n");
+  } else {
+    printf("Filename = %s\n", req->filename);
+  }
+
+  if(success) {
+    printf("Operation Status = success\n");
+  } else {
+    printf("Operation Status = error\n");
+  }
+  printf("--------------------------\n");
+}
+
 void serve(int connfd, int secret) {
   size_t n;
+  bool success;
 
   base_request base_req;
   store_request store_req;
 
-  while(recv(connfd, &base_req, sizeof(base_request), MSG_PEEK) != 0) {
+  while((n = recv(connfd, &store_req, sizeof(store_request), 0)) != 0) {
     switch(base_req.type) {
-      case REQ_STORE:
-        n = recv(connfd, &store_req, sizeof(store_request), 0);
-        printf("server received %d bytes\n", (int)n);
-        printf("sec: %d type: %d filename: %s\n", store_req.base.secret, store_req.base.type, store_req.filename);
-        break;
+    case REQ_STORE:
+      break;
+    case REQ_DELETE:
+      break;
+    case REQ_LIST:
+      break;
+    case REQ_GET:
+      break;
     }
+    log_req(&store_req, success);
+    success = false;
   }
-
-  /* while((n = recv(&connfd, &req, sizeof(base_request), 0)) != 0) { //line:netp:echo:eof */
-  /*   printf("server received %d bytes\n", (int)n); */
-  /*   printf("sec: %d type: %d filename: %s\n", req.secret, req.type, req.filename); */
-  /* } */
 }
