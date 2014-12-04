b077f704e0966e6797cb556d9ef8014df0fbdb17
J David Smith <emallson@archlinux.us>
2014-12-04 14:43:51 -0500

    Added req_id_from_name function

--- common/requests.h
+++ common/requests.h
@@ -1,8 +1,14 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define FNAME_MAX 80
 #define CONTENT_MAX 100*1024
+
+#define REQ_GET 0
+#define REQ_STORE 1
+#define REQ_DELETE 2
+#define REQ_LIST 3
 
 #pragma pack(1)
 typedef struct base_request_struct {
@@ -11,7 +17,6 @@ typedef struct base_request_struct {
 } base_request;
 #pragma pack(0)
 
-#define REQ_STORE 0
 #pragma pack(1)
 typedef struct store_request_struct {
   base_request base;
@@ -26,4 +31,19 @@ size_t store_request_size(store_request* req) {
     + FNAME_MAX * sizeof(char)
     + sizeof(uint32_t)
     + sizeof(char) * req->size;
+}
+
+int req_type_from_name(char* name) {
+  if(strncmp(name, "get", 3) == 0) {
+    return REQ_GET;
+  } else if(strncmp(name, "put", 3) == 0) {
+    return REQ_STORE;
+  } else if(strncmp(name, "del", 3) == 0 ||
+            strncmp(name, "rm", 2) == 0) {
+    return REQ_DELETE;
+  } else if(strncmp(name, "list", 4) == 0 ||
+            strncmp(name, "ls", 2) == 0) {
+    return REQ_LIST;
+  }
+  return -1;
 }
