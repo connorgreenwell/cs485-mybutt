1a99ba7a1f5239555648741eba21117854b0c2a3
J David Smith <emallson@archlinux.us>
2014-12-05 21:21:54 -0500

    Added null byte after file contents during put.
    This fixes a bug where printf'ing the contents would read further on the buffer than intended.
    
    Also added a rule to make symlinks so that the grade script works as expected.

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-CC=gcc -Wall -std=c99
+CC=gcc -Wall -std=gnu99
+DUMMIES=mcput.c mcget.c mclist.c mcdel.c mycloud_server.c
 
-all: buttserver buttclient
+all: buttserver buttclient symlinks
 	chmod u+x mc*
 
 buttserver: common/requests.o server/buttserver.o server/serve.o server/storage.o
@@ -12,6 +13,12 @@ buttclient: common/requests.o client/buttclient.o
 %.o: %.c
 	${CC} $< -lpthread -Icommon -c -o $@
 
-clean:
+symlinks:
+	for dummy in ${DUMMIES}; do [[ -e "$$dummy" ]] || ln -s Makefile "$$dummy"; done
+
+clean-symlinks:
+	for dummy in ${DUMMIES}; do rm -f $$dummy; done
+
+clean: clean-symlinks
 	rm -f */*.o buttserver buttclient
 	chmod u-x mc*
--- client/buttclient.c
+++ client/buttclient.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <netdb.h>
 
 #include "requests.h"
 
@@ -23,7 +24,7 @@ void handle_response(response* resp) {
   }
 }
 
-int send_request(request* req, char* host, int port, response* resp) {
+int send_request(request* req, char* host, char* port, response* resp) {
   int sock;
 
   if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -31,19 +32,29 @@ int send_request(request* req, char* host, int port, response* resp) {
     return -2;
   }
 
-  struct sockaddr_in servAddr;
-  memset(&servAddr, 0, sizeof(servAddr));
-  servAddr.sin_family = AF_INET;
-  servAddr.sin_addr.s_addr = inet_addr(host);
-  servAddr.sin_port = htons(port);
+  struct sockaddr *servAddr;
+  struct addrinfo hints;
+  struct addrinfo *servinfo;
 
-  if(connect(sock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0) {
+  memset(&hints, 0, sizeof(hints));
+
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if(getaddrinfo(host, port, &hints, &servinfo) != 0) {
+    fprintf(stderr, "Failed to resolve hostname\n");
+    return -4;
+  }
+
+  servAddr = servinfo->ai_addr;
+
+  if(connect(sock, servAddr, sizeof(*servAddr)) < 0) {
     fprintf(stderr,"Failed to connect to server\n");
     return -3;
   }
 
   send(sock, req, request_size(req), 0);
-  
+
   if (read(sock, resp, CONTENT_MAX) < 0) {
     close(sock);
     return -4;
@@ -53,11 +64,13 @@ int send_request(request* req, char* host, int port, response* resp) {
 
   close(sock);
 
-  return 1;
+  freeaddrinfo(servinfo);
+
+  return EXIT_SUCCESS;
 }
 
 int main(int argc, char **argv) {
-  int port;
+  char* port;
   char *host;
 
   if (argc < 5) print_usage(argv[0]);
@@ -67,7 +80,7 @@ int main(int argc, char **argv) {
   req.type = req_type_from_name(argv[1]);
 
   host = argv[2];
-  port = atoi(argv[3]);
+  port = argv[3];
   req.secret = atoi(argv[4]);
 
   if (req.type == REQ_LIST) {
@@ -80,6 +93,9 @@ int main(int argc, char **argv) {
   switch (req.type) {
     case REQ_STORE:
       req.size = fread(req.contents, sizeof(char), CONTENT_MAX, stdin);
+      /* null byte */
+      req.contents[req.size] = 0;
+      req.size += 1;
       break;
     case REQ_GET:
       break;
@@ -88,12 +104,12 @@ int main(int argc, char **argv) {
     case REQ_LIST:
       break;
   }
-  
+
   response resp;
 
   if (send_request(&req, host, port, &resp) < 0) {
     exit(-5);
   }
- 
+
   exit(1);
 }
